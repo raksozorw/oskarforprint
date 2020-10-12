93da03e8416be9601fdb56f9ca8420f67f5410fc
Oskar Wroz <oskarwroz@gmail.com>
2020-10-12 14:08:34 +0100

    first commit

---
+++ build/precache-manifest.5491c1a07e3cb897143fe199ea3f3322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d7ba8fb34bec7b4a9d05dd8a5c380e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3af059f80864ec87f1",
+    "url": "/static/css/main.397f93d1.chunk.css"
+  },
+  {
+    "revision": "88a1c8a757bc416cf400",
+    "url": "/static/js/2.9c095eb3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9c095eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3af059f80864ec87f1",
+    "url": "/static/js/main.bb475e74.chunk.js"
+  },
+  {
+    "revision": "21c8fe0c3186847f0f81",
+    "url": "/static/js/runtime-main.7e2c74a6.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const apiPort = process.env.PORT;
+const path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+const stripe = require("stripe")(process.env.STRIPE_SK);
+const reactFiles = path.join(__dirname, "./build");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(reactFiles + "/index.html"));
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  console.log(req.body.data.items);
+  console.log(req.body.data.customer);
+  const { items } = req.body.data;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: items.map((item) => ({
+      quantity: item.quantity,
+      price: item.sku,
+    })),
+    mode: "payment",
+    success_url: "https://example.com/success",
+    cancel_url: "https://example.com/cancel",
+  });
+
+  res.json({ id: session.id });
+});
+
+app.post("/productList", async (req, res) => {
+  const products = await stripe.products.list({
+    limit: 6,
+  });
+  const prices = await stripe.prices.list({
+    limit: 6,
+  });
+
+  const data = { products, prices };
+  res.send(data);
+});
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
